b32d29e608215f7715e583a8db6c224dfbc950d5
Kirtus Leyba <kleyba@asu.edu>
2018-03-02 15:22:41 -0700

    Testing buttons

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //Script to run on homepage canvas
 var canvas = document.getElementById("funCanvas");
 var ctx = canvas.getContext("2d");
+
+var count = 4;
+var speed = 1;
+var xPositions = [10, 50, 80, 120];
+var yPositions = [10, 50, 80, 120];
+
 init();
 
 function init()
@@ -10,12 +16,51 @@ function init()
 	ctx.fillRect(0,0,canvas.width,canvas.height)
 }
 
+function moveToward(indexA, indexB)
+{
+	if(xPositions[indexA] >= xPositions[indexB])
+	{
+		xPositions[indexA] = xPositions[indexA] - speed;
+	}
+	else
+	{
+		xPositions[indexA] = xPositions[indexA] + speed;
+	}
+	if(yPositions[indexA] >= yPositions[indexB])
+	{
+		yPositions[indexA] = yPositions[indexA] - speed;
+	}
+	else
+	{
+		yPositions[indexA] = yPositions[indexA] + speed;
+	}
+}
+
 function update()
 {
-	var x = Math.floor(Math.random()*canvas.width);
-	var y = Math.floor(Math.random()*canvas.height);
-	ctx.fillStyle = "#c4daff";
-	ctx.fillRect(x,y,5,5);
+
+	//Draw
+	for(i = 0; i < count; i++)
+	{
+		var x = xPositions[i];
+		var y = yPositions[i];
+		ctx.fillStyle = "#c4daff";
+		ctx.fillRect(x,y,5,5);
+	}
+
+	//move
+	for(i = 0; i < count; i++)
+	{
+		if(i < count-1)
+		{
+			moveToward(i, i+1);
+		}
+		else
+		{
+			moveToward(i, 0);
+		}
+	}
+
 	requestAnimationFrame(update)
 }
 
